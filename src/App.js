436b6e85eca9ba0edd283e13c2f4c01eed55acf7
Kevin Alexsander De Jesus1 <kjesus1@Lenovo.com>
2023-02-14 10:59:10 -0300

    created starters

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Contact from './views/Contact';
+import Header from './views/Header';
+import Home from './views/Home';
+import Product from './views/Product';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={ <Home/> } />
+        <Route path='/contato' element={ <Contact/> } />
+        <Route path='/produto/:id' element={ <Product/> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
